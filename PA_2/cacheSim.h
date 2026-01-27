bf90c176856a7748336778ab6b73409e0d302677
Ingrid Llorente <143571288+lacroixmeariver@users.noreply.github.com>
2026-01-27 13:44:42 -0800

    Splitting work done into preliminary files and cleaning up the branch by removing PA1 content.

---
+++ PA_2/cacheSim.h
@@ -0,0 +1,37 @@
+#ifndef CACHE_SIM
+#define CACHE_SIM
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*
+* Cache line structure
+* valid: indicates whether the line contains valid data
+* tag: stores the tag portion of the address
+*/
+typedef struct
+{
+    int valid;
+    unsigned int tag;
+} CacheLine;
+
+/*
+* Cache structure
+* lines: array of cache lines
+* num_lines: number of cache lines in the cache
+* block_size: size of each block in bits
+*/
+typedef struct
+{
+    CacheLine* lines;
+    int num_lines;
+    int block_size;
+} Cache;
+
+/* Function prototypes */
+Cache* create_cache(int cache_size, int block_size);
+void free_cache(Cache* cache);
+int access_cache(Cache* cache, unsigned int address);
+void print_stats(int accesses, int hits, int misses);
+
+#endif
