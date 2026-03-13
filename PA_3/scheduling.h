19551d813aed2dae7765574281f441cb3071c952
Ingrid Llorente <143571288+lacroixmeariver@users.noreply.github.com>
2026-03-13 12:23:07 -0700

    Cleaning up scheduling.h

--- PA_3/scheduling.h
+++ PA_3/scheduling.h
@@ -1,10 +1,8 @@
 #ifndef SCHEDULING_H
 #define SCHEDULING_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #define MAX_PROCESSES 100
 
 /* -----------------------------
@@ -26,21 +24,27 @@ typedef struct {
 /* -----------------------------
    Function Prototypes
 -------------------------------- */
+
+// scheduling utilities
+int read_processes(const char* filename, Process processes[]);
+void reset_processes(Process processes[], int n);
+
+// scheduling
+void fcfs(Process processes[], int n);
+void sjf(Process processes[], int n);
+void round_robin(Process processes[], int n, int quantum);
+
+// data display/utilities
+void print_metrics(Process processes[], int n);
+void print_gantt(int pid, int start, int end);
+
+// sorting
 void swap(Process processes[], int i, int j);
 void quick_arrival_sort(Process processes[], int low, int high);
 void quick_burst_sort(Process processes[], int low, int high);
 int arrival_partition(Process processes[], int low, int high);
 int burst_partition(Process processes[], int low, int high);
 
-int read_processes(const char* filename, Process processes[]);
-void reset_processes(Process processes[], int n);
-
-void fcfs(Process processes[], int n);
-void sjf(Process processes[], int n);
-void round_robin(Process processes[], int n, int quantum);
-
-void print_metrics(Process processes[], int n);
-void print_gantt(int pid, int start, int end);
 
 
 #endif //SCHEDULING_H
